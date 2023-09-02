95fd7fa39b4c83ee8a8f6d7fdd485a9b5ceadbda
mwbintang <bintangmuhammadwahid@gmail.com>
2023-09-02 17:55:12 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const app = express()
+const port = 3001
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+ })
