b8592e5b49943baf02038a44fe2757469c778d78
mwbintang <bintangmuhammadwahid@gmail.com>
2023-09-03 12:06:09 +0700

    add cors

--- app.js
+++ app.js
@@ -4,9 +4,11 @@ const app = express()
 const port = 3001
 const route = require('./routes')
 const errorHandler = require("./middlewares/errorHandler")
+const cors = require("cors")
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
+app.use(cors())
 
 app.use('/', route)
 app.use(express.static('./public'))
